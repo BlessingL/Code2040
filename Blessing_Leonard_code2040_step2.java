7f2dcc6a6feaa0582a945e7e1ff28f1ef6669d37
BlessingL <blessingleonard@yahoo.com>
2016-10-27 13:02:00 -0400

    Add files via upload

---
+++ Blessing_Leonard_code2040_step2.java
@@ -0,0 +1,13 @@
+
+public class code2040_step2 {
+
+	public static void main(String[] args) {
+		String word = "nprgsqwa";	//the string to be reversed
+		for (int i = word.length()-1; i >= 0; i--){
+			System.out.print(word.charAt(i));	//Prints out the initial string in reverse
+		}
+		
+
+	}
+
+}
